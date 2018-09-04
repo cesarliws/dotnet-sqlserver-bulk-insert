4d7fdf190e7a989eb875474b310c50d94cce3d3c
Cesar Romero Silva <cesar.silva@hbsis.com.br>
2018-09-04 17:32:31 -0300

    + links referência

--- BulkOperation.cs
+++ BulkOperation.cs
@@ -5,10 +5,17 @@ using System.Threading.Tasks;
 
 namespace BulkOperations
 {
-    /*
+    /* TODO 
+     * https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy?redirectedfrom=MSDN&view=netframework-4.7.2
+     * https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy.writetoserver?view=netframework-4.7.2&viewFallbackFrom=netcore-2.1
      * https://docs.microsoft.com/pt-br/dotnet/framework/data/adonet/sql/single-bulk-copy-operations
      * https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/sql/bulk-copy-example-setup?view=netframework-4.7.2
-     * TODO https://github.com/mgravell/fast-member
+     * https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy?f1url=https%3A%2F%2Fmsdn.microsoft.com%2Fquery%2Fdev15.query%3FappId%3DDev15IDEF1%26l%3DEN-US%26k%3Dk(System.Data.SqlClient.SqlBulkCopy);k(SolutionItemsProject);k(TargetFrameworkMoniker-.NETFramework,Version%3Dv4.7);k(DevLang-csharp)%26rd%3Dtrue&view=netframework-4.7.2
+     * https://github.com/mgravell/fast-member
+     *
+     * Update if exist https://stackoverflow.com/questions/12521692/c-sharp-bulk-insert-sqlbulkcopy-update-if-exists/12535726
+     * https://www.databasejournal.com/features/mssql/article.php/3739131/UPSERT-Functionality-in-SQL-Server-2008.htm
+     * https://stackoverflow.com/questions/4889123/any-way-to-sqlbulkcopy-insert-or-update-if-exists
      */
 
     public static class BulkOperation
@@ -41,6 +48,7 @@ namespace BulkOperations
 
         private static void MapEntity(this SqlBulkCopy bulk)
         {
+            // https://stackoverflow.com/questions/40470357/importing-a-csv-using-sqlbulkcopy-with-asp-net-core
             bulk.DestinationTableName = "Customer";
             bulk.ColumnMappings.Add(nameof(Customer.Id), "Id");
             bulk.ColumnMappings.Add(nameof(Customer.FirstName), "FirstName");
